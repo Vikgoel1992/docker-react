538c8c894a150744e0f9644793f2382106fc68d0
Ubuntu <ubuntu@ip-172-31-91-120.ec2.internal>
2020-12-22 04:17:42 +0000

    changed txt

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           <code>Hi there</code> Byee Yall.
+           <code>Hi there</code> Byee Yall changed.
         </p>
         <a
           className="App-link"
